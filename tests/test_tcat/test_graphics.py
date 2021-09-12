2db1a4b27108b265a6dd9584312f1ccc615b2ef5
Harrison Totty <harrison.totty@centro.net>
2021-09-12 12:53:01 -0500

    Partial work on graphics tests

---
+++ tests/test_tcat/test_graphics.py
@@ -0,0 +1,13 @@
+'''
+Tests Graphics Objects
+'''
+
+#import tcat.graphics as tg
+
+#from . import T
+
+def test_balance_candle_plot():
+    '''
+    Tests various options of the balance candle plot.
+    '''
+    #tg.balance_candle_plot(T)
